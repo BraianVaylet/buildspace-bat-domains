7835ddabd15860473b2fdb614f67150420ea8d1f
Braian Vaylet <braianvaylet@gmail.com>
2022-04-14 11:54:55 -0300

    working in ENS/NFT domain

---
+++ scripts/run.js
@@ -0,0 +1,30 @@
+const main = async () => {
+  const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+  // We pass in "ninja" to the constructor when deploying
+  const domainContract = await domainContractFactory.deploy("bat");
+  await domainContract.deployed();
+
+  console.log("Contract deployed to:", domainContract.address);
+
+  // We're passing in a second variable - value. This is the moneyyyyyyyyyy
+  let txn = await domainContract.register("mortal",  {value: hre.ethers.utils.parseEther('0.1')});
+  await txn.wait();
+
+  const address = await domainContract.getAddress("mortal");
+  console.log("Owner of domain mortal:", address);
+
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
